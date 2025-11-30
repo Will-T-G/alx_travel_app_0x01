53a74115186bcdf275a3123d0c219ea7ddec50be
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-30 17:52:28 +0200

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,1 +1,20 @@
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for CRUD operations on Listings.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for CRUD operations on Bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
